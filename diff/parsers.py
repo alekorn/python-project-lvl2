ac46d357fd537bee17d07df3d7359a9d61afeafb
alekorn <entkorn@gmail.com>
2020-01-27 23:53:14 +0300

    refactoring parser

---
+++ diff/parsers.py
@@ -0,0 +1,13 @@
+import json
+import yaml
+
+
+def parse(first_file, second_file, format):
+    if format == 'json':
+        first_json = json.load(open(first_file))
+        second_json = json.load(open(second_file))
+        return first_json, second_json
+    if format == 'yml':
+        first_yaml = yaml.load(open(first_file), Loader=yaml.SafeLoader)
+        second_yaml = yaml.load(open(second_file), Loader=yaml.SafeLoader)
+        return first_yaml, second_yaml
