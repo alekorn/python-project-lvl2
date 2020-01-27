1109529209f1453bf1dbeef86fc09815a55460ca
alekorn <entkorn@gmail.com>
2020-01-27 23:57:52 +0300

    add print

--- diff/scripts/gendiff.py
+++ diff/scripts/gendiff.py
@@ -3,8 +3,8 @@ from diff.engine import generate_diff, arg_parse
 
 
 def main():
-    first_file, second_file = arg_parse()
-    generate_diff(first_file, second_file)
+    first_file, second_file, format = arg_parse()
+    print(generate_diff(first_file, second_file, format))
 
 
 if __name__ == '__main__':
