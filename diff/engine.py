1bc2ad8daf52646d68e5c8a630bd533cf7abf120
alekorn <entkorn@gmail.com>
2020-02-10 21:02:12 +0300

    add recursion json diff and rendering

--- diff/engine.py
+++ diff/engine.py
@@ -1,6 +1,12 @@
 from diff.parsers import parse
 import argparse
 
+ADD = '  + '
+DEL = '  - '
+TAB = '    '
+END = '}'
+START = '{'
+
 
 def arg_parse():
     parser = argparse.ArgumentParser(description='Generate diff')
@@ -17,19 +23,92 @@ def arg_parse():
 
 def generate_diff(first_file, second_file, format):
     first, second = parse(first_file, second_file, format)  # в скрипт
-    added_keys = sorted(first.keys() - second.keys())
-    deleted_keys = sorted(second.keys() - first.keys())
-    other_keys = sorted(first.keys() & second.keys())
-    output_string = '{\n'
+    return get_diff(first, second)
+
+
+def get_diff(first, second, new_dict=None, parent=('has_child', None)):
+    if new_dict is None:
+        new_dict = {('has_child', None): {}}
+    if isinstance(first, dict) and isinstance(second, dict):
+        deleted, added, not_changed, changed = compare_keys(first, second)
+        for key in added:
+            new_dict[parent][('added', key)] = second[key]
+        for key in deleted:
+            new_dict[parent][('deleted', key)] = first[key]
+        for key in not_changed:
+            new_dict[parent][('not_changed', key)] = first[key]
+        for key in changed:
+            if isinstance(first[key], dict) and isinstance(second[key], dict):
+                new_dict[parent][('has_child', key)] = {}
+                get_diff(
+                        first[key],
+                        second[key],
+                        new_dict[parent],
+                        ('has_child', key)
+                        )
+            else:
+                new_dict[parent][('changed', key)] = (first[key], second[key])
+        return new_dict
+
+
+def compare_keys(first_dict, second_dict):
+    added_keys = sorted(first_dict.keys() - second_dict.keys())
+    deleted_keys = sorted(second_dict.keys() - first_dict.keys())
+    other_keys = sorted(first_dict.keys() & second_dict.keys())
+    not_changed = []
+    changed = []
     for key in other_keys:
-        if first[key] == second[key]:
-            output_string += f'    {key}: {first[key]}\n'
+        if first_dict[key] == second_dict[key]:
+            not_changed.append(key)
         else:
-            output_string += f'  + {key}: {first[key]}\n'
-            output_string += f'  - {key}: {second[key]}\n'
-    for key in added_keys:
-        output_string += f'  - {key}: {first[key]}\n'
-    for key in deleted_keys:
-        output_string += f'  + {key}: {second[key]}\n'
-    output_string += '}'
-    return output_string
+            changed.append(key)
+    return added_keys, deleted_keys, not_changed, changed
+
+
+def rendering(new_dict, new_list=None, tab=''):
+    if new_list is None:
+        new_list = []
+    if new_list:
+        tab += '    '
+    for key in new_dict:
+        if key[0] == 'has_child' and key[1]:
+            new_list.append(f'{tab}{TAB}{key[1]}: {START}')
+        if key[0] == 'has_child':
+            rendering(new_dict[key], new_list, tab)
+    for key in new_dict:
+        if key[0] == 'added' and isinstance(new_dict[key], dict):
+            new_list.append(f'{tab}{ADD}{key[1]}: {START}')
+            new_list.append(f'{TAB * 2 + tab}{stringify(new_dict[key])}')
+            new_list.append(f'{TAB}{tab}{END}')
+        elif key[0] == 'added':
+            new_list.append(f'{tab}{ADD}{key[1]}: {new_dict[key]}')
+        if key[0] == 'deleted' and isinstance(new_dict[key], dict):
+            new_list.append(f'{tab}{DEL}{key[1]}: {START}')
+            new_list.append(f'{TAB * 2 + tab}{stringify(new_dict[key])}')
+            new_list.append(f'{TAB}{tab}{END}')
+        elif key[0] == 'deleted':
+            new_list.append(f'{tab}{DEL}{key[1]}: {new_dict[key]}')
+        if key[0] == 'not_changed' and isinstance(new_dict[key], dict):
+            new_list.append(f'{tab}{TAB}{key[1]}: {START}')
+            new_list.append(f'{TAB * 2 + tab}{stringify(new_dict[key])}')
+            new_list.append(f'{TAB}{tab}{END}')
+        elif key[0] == 'not_changed':
+            new_list.append(f'{tab}{TAB}{key[1]}: {new_dict[key]}')
+        if key[0] == 'changed' and isinstance(new_dict[key], dict):
+            new_list.append(f'{tab}{TAB}{key[1]}: {START}')
+            new_list.append(f'{tab}{DEL}{key[1]}: {new_dict[key][0]}')
+            new_list.append(f'{tab}{ADD}{key[1]}: {new_dict[key][1]}')
+            new_list.append(f'{TAB}{tab}{END}')
+        elif key[0] == 'changed':
+            new_list.append(f'{tab}{DEL}{key[1]}: {new_dict[key][0]}')
+            new_list.append(f'{tab}{ADD}{key[1]}: {new_dict[key][1]}')
+    if key[1]:
+        new_list.append(f'{tab}{END}')
+    return '{\n' + '\n'.join(new_list)
+
+
+def stringify(new_dict):
+    out_string = ''
+    for key in new_dict:
+        out_string += f'{key}: {new_dict[key]}'
+    return (out_string)
