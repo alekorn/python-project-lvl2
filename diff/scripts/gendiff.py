74fc4f809ac52375509f9ed90e512ed0467757cd
alekorn <entkorn@gmail.com>
2020-02-10 21:04:35 +0300

    update run script

--- diff/scripts/gendiff.py
+++ diff/scripts/gendiff.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from diff.engine import generate_diff, arg_parse
+from diff.engine import generate_diff, arg_parse, rendering
 
 
 def main():
     first_file, second_file, format = arg_parse()
-    print(generate_diff(first_file, second_file, format))
+    print(rendering(generate_diff(first_file, second_file, format)))
 
 
 if __name__ == '__main__':
