2703b6d85097d5a692b14421c650de59f07a687d
alekorn <entkorn@gmail.com>
2020-02-10 23:26:24 +0300

    update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -6,13 +6,10 @@ file2 = open('./tests/fixtures/exp_recursion.txt', 'r')
 exp_recursion = file2.read().rstrip()
 
 
-def test_generate_diff_json():
-    assert rendering(generate_diff('./tests/fixtures/before.json', './tests/fixtures/after.json', 'json')) == exp
-
-
-def test_generate_diff_yaml():
-    assert rendering(generate_diff('./tests/fixtures/before.yml', './tests/fixtures/after.yml', 'yml')) == exp
-
-
 def test_generate_diff_recursion_json():
     assert rendering(generate_diff('./tests/fixtures/before_recursion.json', './tests/fixtures/after_recursion.json', 'json')) == exp_recursion
+
+def test_generate_diff_json():
+    assert rendering(generate_diff('./tests/fixtures/before.json', './tests/fixtures/after.json', 'json')) == exp
+def test_generate_diff_yaml():
+    assert rendering(generate_diff('./tests/fixtures/before_recursion.yaml', './tests/fixtures/after_recursion.yaml', 'yaml')) == exp_recursion
