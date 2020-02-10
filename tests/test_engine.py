3162466b26af22f30cd41088c922d823e2a6b208
alekorn <entkorn@gmail.com>
2020-02-10 21:01:07 +0300

    add and update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,12 +1,18 @@
-from diff.engine import generate_diff
+from diff.engine import generate_diff, rendering
 
-file = open('./tests/fixtures/exp_json.txt', 'r')
-result = file.read().rstrip()
+file1 = open('./tests/fixtures/exp_json.txt', 'r')
+exp = file1.read().rstrip()
+file2 = open('./tests/fixtures/exp_recursion.txt', 'r')
+exp_recursion = file2.read().rstrip()
 
 
 def test_generate_diff_json():
-    assert generate_diff('./tests/fixtures/before.json', './tests/fixtures/after.json', 'json') == result
+    assert rendering(generate_diff('./tests/fixtures/before.json', './tests/fixtures/after.json', 'json')) == exp
 
 
 def test_generate_diff_yaml():
-    assert generate_diff('./tests/fixtures/before.yml', './tests/fixtures/after.yml', 'yml') == result
+    assert rendering(generate_diff('./tests/fixtures/before.yml', './tests/fixtures/after.yml', 'yml')) == exp
+
+
+def test_generate_diff_recursion_json():
+    assert rendering(generate_diff('./tests/fixtures/before_recursion.json', './tests/fixtures/after_recursion.json', 'json')) == exp_recursion
