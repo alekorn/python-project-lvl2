9170b8742a2f2ca1ae355779afa7f27a48fcd907
alekorn <entkorn@gmail.com>
2020-02-12 22:49:39 +0300

    update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,4 +1,5 @@
-from diff.engine import generate_diff, rendering
+from diff.engine import generate_diff
+from diff.formatters import json
 
 file1 = open('./tests/fixtures/exp_json.txt', 'r')
 exp = file1.read().rstrip()
@@ -7,9 +8,12 @@ exp_recursion = file2.read().rstrip()
 
 
 def test_generate_diff_recursion_json():
-    assert rendering(generate_diff('./tests/fixtures/before_recursion.json', './tests/fixtures/after_recursion.json', 'json')) == exp_recursion
+    assert json.rendering(generate_diff('./tests/fixtures/before_recursion.json', './tests/fixtures/after_recursion.json', 'json')) == exp_recursion
+
 
 def test_generate_diff_json():
-    assert rendering(generate_diff('./tests/fixtures/before.json', './tests/fixtures/after.json', 'json')) == exp
+    assert json.rendering(generate_diff('./tests/fixtures/before.json', './tests/fixtures/after.json', 'json')) == exp
+
+
 def test_generate_diff_yaml():
-    assert rendering(generate_diff('./tests/fixtures/before_recursion.yaml', './tests/fixtures/after_recursion.yaml', 'yaml')) == exp_recursion
+    assert json.rendering(generate_diff('./tests/fixtures/before_recursion.yaml', './tests/fixtures/after_recursion.yaml', 'yaml')) == exp_recursion
