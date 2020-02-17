34080d153a153df5c82adc6ab720287dffe2d2e7
alekorn <entkorn@gmail.com>
2020-02-17 11:39:33 +0300

    refactoring

---
+++ diff/formatters/json_format.py
@@ -0,0 +1,71 @@
+ADD = '  + '
+DEL = '  - '
+TAB = '    '
+END = '}'
+START = '{'
+
+
+def rendering(new_dict, new_list=None, tab=''):
+    if new_list is None:
+        new_list = []
+    else:
+        tab += '    '
+    for key in new_dict:
+        if key[0] == 'added' and isinstance(new_dict[key], dict):
+            new_list.append(
+                    f'{tab}{ADD}{key[1]}: {START}\n'
+                    f'{stringify(new_dict[key], tab)}\n'
+                    f'{TAB}{tab}{END}'
+                    )
+        elif key[0] == 'added':
+            new_list.append(f'{tab}{ADD}{key[1]}: {truefy(new_dict[key])}')
+        if key[0] == 'deleted' and isinstance(new_dict[key], dict):
+            new_list.append(
+                    f'{tab}{DEL}{key[1]}: {START}\n'
+                    f'{stringify(new_dict[key], tab)}\n'
+                    f'{TAB}{tab}{END}'
+                    )
+        elif key[0] == 'deleted':
+            new_list.append(f'{tab}{DEL}{key[1]}: {truefy(new_dict[key])}')
+        if key[0] == 'not_changed' and isinstance(new_dict[key], dict):
+            new_list.append(
+                    f'{tab}{TAB}{key[1]}: {START}\n'
+                    f'{stringify(new_dict[key], tab)}\n'
+                    f'{TAB}{tab}{END}'
+                    )
+        elif key[0] == 'not_changed':
+            new_list.append(f'{tab}{TAB}{key[1]}: {truefy(new_dict[key])}')
+        if key[0] == 'changed' and isinstance(new_dict[key], dict):
+            new_list.append(
+                    f'{tab}{TAB}{key[1]}: {START}\n'
+                    f'{tab}{DEL}{key[1]}: {truefy(new_dict[key][0])}\n'
+                    f'{tab}{ADD}{key[1]}: {truefy(new_dict[key][1])}\n'
+                    f'{TAB}{tab}{END}'
+                    )
+        elif key[0] == 'changed':
+            new_list.append(
+                    f'{tab}{DEL}{key[1]}: {truefy(new_dict[key][0])}\n'
+                    f'{tab}{ADD}{key[1]}: {truefy(new_dict[key][1])}'
+                    )
+        if key[0] == 'has_child' and key[1]:
+            new_list.append(f'{tab}{TAB}{key[1]}: {START}')
+        if key[0] == 'has_child':
+            rendering(new_dict[key], new_list, tab)
+    if key[1]:
+        new_list.append(f'{tab}{END}')
+    return '{\n' + '\n'.join(new_list)
+
+
+def stringify(new_dict, tab):
+    out_string = ''
+    for key in new_dict:
+        out_string += f'{TAB * 2 + tab}{key}: {truefy(new_dict[key])}\n'
+    return out_string.rstrip()
+
+
+def truefy(string):
+    if string is True:
+        return 'true'
+    if string is None:
+        return 'none'
+    return string
--- diff/scripts/gendiff.py
+++ diff/scripts/gendiff.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from diff.engine import generate_diff, arg_parse
-from diff.formatters import json
+from diff.formatters import json_format
 
 
 def main():
     first_file, second_file, format = arg_parse()
-    print(json.rendering(generate_diff(first_file, second_file, format)))
+    print(json_format.rendering(generate_diff(first_file, second_file, format)))
 
 
 if __name__ == '__main__':
