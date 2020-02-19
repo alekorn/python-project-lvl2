c428e5a2e87b0a88b3b08036307df4cc080c1326
alekorn <entkorn@gmail.com>
2020-02-19 21:21:03 +0300

    fix codeclimate warnings

--- diff/parsers.py
+++ diff/parsers.py
@@ -3,15 +3,11 @@ import yaml
 
 
 def parse(first_file, second_file, format):
-    if format == 'json':
+    if format == 'json' or format == 'plain':
         first_json = json.load(open(first_file))
         second_json = json.load(open(second_file))
         return first_json, second_json
-    if format == 'yaml' or format == 'yml':
+    elif format == 'yaml' or format == 'yml':
         first_yaml = yaml.load(open(first_file), Loader=yaml.SafeLoader)
         second_yaml = yaml.load(open(second_file), Loader=yaml.SafeLoader)
         return first_yaml, second_yaml
-    if format == 'plain':
-        first_json = json.load(open(first_file))
-        second_json = json.load(open(second_file))
-        return first_json, second_json
