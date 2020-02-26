57b1257860ecc8e64285b28e5c90cb47f4988f08
alekorn <entkorn@gmail.com>
2020-02-26 15:33:18 +0300

    refactoring: move loads func to fileloader.py

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -1,5 +1,4 @@
-import json
-import yaml
+from gendiff.fileloader import json_load, yaml_load
 import os
 
 
@@ -8,10 +7,10 @@ def parse(first_file, second_file):
     second_ext = os.path.splitext(second_file)[1]
     if first_ext == '.json' and second_ext == '.json':
         files_format = 'json'
-        first_dict = json.load(open(first_file))
-        second_dict = json.load(open(second_file))
+        first_dict = json_load(first_file)
+        second_dict = json_load(second_file)
     elif first_ext == '.yml' and second_ext == '.yml':
         files_format = 'yaml'
-        first_dict = yaml.load(open(first_file), Loader=yaml.SafeLoader)
-        second_dict = yaml.load(open(second_file), Loader=yaml.SafeLoader)
+        first_dict = yaml_load(first_file)
+        second_dict = yaml_load(second_file)
     return first_dict, second_dict, files_format
