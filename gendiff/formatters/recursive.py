0e4ebc23f81937e14e47c462b201dad86656c174
alekorn <entkorn@gmail.com>
2020-03-03 14:03:52 +0300

    refactoring: add variables status_key and value_key

--- gendiff/formatters/recursive.py
+++ gendiff/formatters/recursive.py
@@ -1,3 +1,4 @@
+from gendiff.constants import ADDED, DELETED, CHANGED, NOT_CHANGED, HAS_CHILDS
 ADD = '  + '
 DEL = '  - '
 TAB = '    '
@@ -7,37 +8,39 @@ START = '{'
 
 def rendering(dic, out_list=None, tab=''):
     if out_list is None:
-        out_list = []
+        out_list = [START]
     else:
         tab += '    '
-    for key in dic:
-        if dic[key]['status'] == 'added':
-            out_list.append(stringify(dic[key]['value'], ADD, key, tab))
-        elif dic[key]['status'] == 'deleted':
-            out_list.append(stringify(dic[key]['value'], DEL, key, tab))
-        elif dic[key]['status'] == 'not_changed':
-            out_list.append(stringify(dic[key]['value'], TAB, key, tab))
-        elif dic[key]['status'] == 'changed':
-            out_list.append(f'{tab}{DEL}{key}: {truefy(dic[key]["old_value"])}')
-            out_list.append(f'{tab}{ADD}{key}: {truefy(dic[key]["value"])}')
-        elif dic[key]['status'] == 'has_child':
+    for key, value in dic.items():
+        status_key = value['status']
+        value_key = value['value']
+        if status_key == ADDED:
+            out_list.extend(stringify(value_key, ADD, key, tab))
+        elif status_key == DELETED:
+            out_list.extend(stringify(value_key, DEL, key, tab))
+        elif status_key == NOT_CHANGED:
+            out_list.extend(stringify(value_key, TAB, key, tab))
+        elif status_key == CHANGED:
+            out_list.append(f'{tab}{DEL}{key}: {truefy(value["old_value"])}')
+            out_list.append(f'{tab}{ADD}{key}: {truefy(value_key)}')
+        elif status_key == HAS_CHILDS:
             out_list.append(f'{tab}{TAB}{key}: {START}')
-            rendering(dic[key]['value'], out_list, tab)
+            rendering(value_key, out_list, tab)
     if key:
         out_list.append(f'{tab}{END}')
-    return '{\n' + '\n'.join(out_list)
+    return '\n'.join(out_list)
 
 
 def stringify(inp, status, current_key, tab):
-    out_string = ''
+    out_list = []
     if isinstance(inp, dict):
-        out_string += f'{tab}{status}{current_key}: {START}\n'
+        out_list.append(f'{tab}{status}{current_key}: {START}')
         for key in inp:
-            out_string += f'{TAB * 2 + tab}{key}: {truefy(inp[key])}\n'
-        out_string += f'{TAB}{tab}{END}'
+            out_list.append(f'{TAB * 2 + tab}{key}: {truefy(inp[key])}')
+        out_list.append(f'{TAB}{tab}{END}')
     else:
-        out_string += f'{tab}{status}{current_key}: {truefy(inp)}'
-    return out_string
+        out_list.append(f'{tab}{status}{current_key}: {truefy(inp)}')
+    return out_list
 
 
 def truefy(string):
