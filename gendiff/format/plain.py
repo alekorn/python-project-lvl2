f4fd2ea76c6486d7f022735fa02f00e1e5a7a64f
alekorn <entkorn@gmail.com>
2020-03-09 15:19:41 +0300

    import and use constants

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -1,10 +1,13 @@
+from gendiff.constants import ADDED, DELETED, CHANGED, HAS_CHILDS
+
+
 def format(dic, out_list=None, path=''):
     if out_list is None:
         out_list = []
     for key, value in dic.items():
         status_key = value['status']
         value_key = value['value']
-        if status_key == 'added':
+        if status_key == ADDED:
             if isinstance(value_key, dict):
                 new_val = 'complex value'
             else:
@@ -12,14 +15,14 @@ def format(dic, out_list=None, path=''):
             out_list.append(
                     f"Property '{path}{key}' was added with value: '{new_val}'"
                     )
-        elif status_key == 'deleted':
+        elif status_key == DELETED:
             out_list.append(f"Property '{path}{key}' was removed")
-        elif status_key == 'changed':
+        elif status_key == CHANGED:
             out_list.append(
                     f"Property '{path}{key}' was changed. "
                     f"From '{value['old_value']}' to '{value_key}'"
                     )
-        elif status_key == 'has_child':
+        elif status_key == HAS_CHILDS:
             new_path = path + key + '.'
             format(value_key, out_list, new_path)
     out_list.sort()
